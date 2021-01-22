b3e50433397810646287557f7ab2626223161f47
Anton Fomin <au_fomin@icloud.com>
2021-01-22 18:52:25 +0300

    Add final name

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,4 +1,4 @@
-package com.mycompany.app;
+package com.devopslab;
 
 /**
  * Hello world!
--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -1,4 +1,4 @@
-package com.mycompany.app;
+package com.devopslab;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
